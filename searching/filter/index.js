5058c28f15223ceaee3df8bab08e96b79de724b9
starydv7 <pawanstar72926@gmail.com>
2022-09-10 23:17:10 +0530

    bot able to get data on screen

--- searching/filter/index.js
+++ searching/filter/index.js
@@ -11,58 +11,59 @@ let products = {
             productsName: "Beige shorts",
             category: "Bottom",
             price: "36",
-            image: ""
+            image: "https://m.media-amazon.com/images/I/71pyxqMnLUL._UL1500_.jpg"
         },
       
         {
             productsName: "sporty smartwatch",
             category: "watch",
             price: "305",
-            image: ""
+            image: "https://m.media-amazon.com/images/I/71pyxqMnLUL._UL1500_.jpg"
         },
     
         {
             productsName: "basics knee",
             category: "Topwear",
             price: "300",
-            image: ""
+            image: "https://m.media-amazon.com/images/I/71pyxqMnLUL._UL1500_.jpg"
         },
       
         {
             productsName: "Black leevis",
             category: "Jacket",
             price: "3000",
-            image: ""
+            image: "https://m.media-amazon.com/images/I/71pyxqMnLUL._UL1500_.jpg"
         },
         
         {
             productsName: "Stylish",
             category: "Bottom",
             price: "30055",
-            image: ""
+            image: "https://m.media-amazon.com/images/I/71pyxqMnLUL._UL1500_.jpg"
         },
           
         {
             productsName: "Brown",
             category: "Jacket",
             price: "4780",
-            image: ""
+            image: "https://m.media-amazon.com/images/I/71pyxqMnLUL._UL1500_.jpg"
         },
     
         {
             productsName: "Regular red Tshirts",
             category: "TopWear",
             price: "30",
-            image: ""
+            image: "https://m.media-amazon.com/images/I/71pyxqMnLUL._UL1500_.jpg"
         },
 
     ]
 };
+console.log(products.data);
 
 for (let i of products.data) {
     //create card
     let card = document.createElement("div");
-    card.classList.add("card", i.category);
+    card.classList.add("card", 'i.category');
 
     //image div
 
@@ -72,7 +73,9 @@ for (let i of products.data) {
 
     let image = document.createElement("img");
     imgContainer.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
     card.appendChild(imgContainer);
+    
 
     document.getElementById("products").appendChild(card);
 }
