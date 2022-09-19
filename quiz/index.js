02d03b413f2b45b3210b7bdb9e06b3460091f052
starydv7 <pawanstar72926@gmail.com>
2022-09-19 22:57:02 +0530

    login

---
+++ quiz/index.js
@@ -0,0 +1,129 @@
+// function value(b) {
+//     const a = 100;
+//     a = b
+//     return a
+// }
+// console.log(value(100));
+
+// let fname = "masai"
+// let lname = "school"
+// const person = { fname, lname }
+// console.log(person)
+
+// typeof [1, 1, 1]===typeof { val: 1, val: 1, val: 1 }
+// // /console.log()/
+// var name = "ajay"
+// function printDetails() {
+//     console.log(this.name)
+// }
+// var person = {
+//     name: "haren",
+//     company: "masai",
+//     print:printDetails()
+// }
+// console.log(person);
+
+// /function getName() {
+//     var status = true
+//     if (status) {
+//         const name="masai"
+//     }
+//     console.log(name);
+// }
+// getName();/
+// const prod = (x) => {
+//     x = x + 5
+//     return inner => (
+//         x* inner
+//     )
+// }
+// console.log(prod(5)(10))
+
+
+
+// var x = 100;
+// function alpha() {
+//     var x = 200
+//     function beta() {
+//         console.log(x)
+//     }
+//     return beta
+// }
+// var gamma = alpha()
+// gamma()
+
+// const arr = [10, 12, 15, 21];
+// for (var i = 0; i < arr.length; i++){
+//     setTimeout(function () {
+//         console.log("index: " + i + ',element' + arr[i]);
+//     }, 3000);
+// }
+
+// function alpha() {
+//     var x = 100;
+//     this.beta()
+// }
+// function beta() {
+//     var x = 200
+//     console.log(this.x)
+// }
+// alpha()
+
+// name = "masai"
+// var name
+// console.log(name)
+//
+// printName('sdjajd')
+// var printName = function (name) {
+//     console.log("hello",name)
+// }
+// var arr = [1, 2, 3, 4, 5]
+// arr.map(a->a*5)
+
+// function getName() {
+//     var status = true
+//     if (status) {
+//         var name="masai"
+//     }
+//     console.log(name)
+// }
+// getName()
+// function alpha(a = 100)
+//     {...}
+
+// console.log(alpha)
+// const isAuth
+// console.log(isAuth)
+// name = x || 'name'
+// console.log(name)
+// var x="masai"
+// var str = 'masai'
+// var { length } = str
+// console.log(str)
+// const email = 'sdada'
+// let name = 'admin'
+// email = "sadasd"
+// name='superf'
+// printName('shsds')
+// var printName
+// function printName(name) {
+//     console.log("hello",name)
+// }
+// printName = function (name) {
+//     console.log("hey",name)
+// }
+
+// function printDetails() {
+//     console.log(this.name)
+// }
+// var person = {
+//     name: "haren",
+//     company: "masai",
+//     print:printDetails
+// }
+// person.print()
+// var name='ajay'
+const sum = (x, y) => {
+    x = x && 5
+    y=y
+}
